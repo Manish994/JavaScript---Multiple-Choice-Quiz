ad4054da9c1fd1dc8a739cbb89fae1192bc264bd
Manish <manihs@gmail.com>
2020-09-07 15:54:20 +0545

    Working JS, complete 80% on Quiz Section

--- quiz.js
+++ quiz.js
@@ -8,6 +8,7 @@ const choiceBElement = document.getElementById("B");
 const choiceCElement = document.getElementById("C");
 const counterElement = document.getElementById("counter");
 const timeGaugeElement = document.getElementById("timeGauge");
+const progressElement = document.getElementById("progress");
 const scoreContainerElement = document.getElementById("scoreContainer");
 
 //
@@ -40,6 +41,11 @@ let questions = [
 
 //Creates Some Variables
 const lastQuestion = questions.length - 1;
+let TIMER;
+let count = 0;
+let questionTime = 10;
+let gaugeWidth = 150;
+let gaugeUnit = gaugeWidth / questionTime;
 
 //Track Current Question
 let runningQuestion = 0;
@@ -52,7 +58,34 @@ function renderQuestion() {
   choiceBElement.innerHTML = `${q.choiceB};`;
   choiceCElement.innerHTML = `${q.choiceC};`;
 }
-//Before displaying our questions hide startDiv
-startElement.style.display = "none";
-renderQuestion();
-quizElement.style.display = "block";
+
+//When click on startDivElement then display Quiz Section
+startElement.addEventListener("click", startQuiz);
+function startQuiz() {
+  //Before displaying our questions hide startDiv
+  startElement.style.display = "none";
+  renderQuestion();
+  quizElement.style.display = "block";
+  renderProgress();
+  TIMER = setInterval(renderCounter, 1000);
+}
+
+//Render Progress dot
+function renderProgress() {
+  for (let index = 0; index <= lastQuestion; index++) {
+    //Addition Assignment Operator(+=)
+    progressElement.innerHTML += `<div class="prog" id="${index}"></div>`;
+  }
+}
+
+//Time Counter
+//Per Second Increment width px also increment
+function renderCounter() {
+  if (count <= questionTime) {
+    counterElement.innerHTML = count;
+    timeGaugeElement.style.width = gaugeUnit * count + "px";
+    count++;
+  } else {
+    count = 0;
+  }
+}
